a7350cad04bd61ea72f535ef13186357ce0d56d9
Niicck <nickivons@yahoo.com>
2015-11-25 21:55:11 -0800

    Add README text

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # git-shortcut-generator
+
+Easily add all of the git keyboard shortcuts from "Git Immersion Labs" automatically.
+Pull the repo, make sure git-shortcut-generator.sh is in your working directory, 
+and run the shell script by typing in:
+
+    "source git-shortcut-generator.sh"
+
+And you're good to go.
--- git-shortcut-generator.sh
+++ git-shortcut-generator.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 #Creates git aliases
 
-#Create sublime shortcut
-#Might need fixing if ~/bin doesn't exist
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
-
 #Unix shortcuts
 alias gs='git status '
 alias ga='git add '
 alias gb='git branch '
-alias gc='git commit'
 alias gd='git diff'
+alias gc='git commit'
 alias go='git checkout '
 alias gk='gitk --all&'
 alias gx='gitx --all'
@@ -23,3 +19,8 @@ git config --global alias.br branch
 git config --global alias.hist log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short
 git config --global alias.type cat-file -t
 git config --global alias.dump cat-file -p
+
+
+#Create sublime shortcut
+#Might need fixing if ~/bin doesn't exist
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
