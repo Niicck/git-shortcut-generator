8a56bdfc70fa5a7af0f1ba021502af9452431180
Niicck <nickivons@yahoo.com>
2015-12-01 08:35:50 -0800

    Removed unneeded subl alias

--- git-shortcut-generator.sh
+++ git-shortcut-generator.sh
@@ -19,8 +19,3 @@ git config --global alias.br branch
 git config --global alias.hist log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short
 git config --global alias.type cat-file -t
 git config --global alias.dump cat-file -p
-
-
-#Create sublime shortcut
-#Might need fixing if ~/bin doesn't exist
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
